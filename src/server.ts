61257057597b3fb95fdfecad0bc43a5914bdff62
Leonardo Sousa <leonardolsousa5@gmail.com>
2021-06-21 20:43:19 -0300

    Configurando o database

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,11 @@
-import express, { response } from "express";
+import "reflect-metadata";
+import express from "express";
+
+import "./database";
 
 const app = express();
-/**
- * GET      => Buscar uma informação
- * POST     => Inserir/criar uma informação
- * PUT      => Alterar una informação 
- * DELETE   => Remover um dado
- * PATCH    => Alterar uma informação específica (alterar somente a senha, por exemplo)
- */
+
 app.get("/test", (req, res) => {
-    //Request  => Entrando
-    //Response => Saindo
     return res.send('Olá NLW Together')
 })
 
@@ -19,7 +14,3 @@ app.post("/test-post", (req, res) => {
 })
 
 app.listen(3333, () => console.log("Server is running on NLW"));
-
-/**
- * Código Aula 1 - #together
- */
